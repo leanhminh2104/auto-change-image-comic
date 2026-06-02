a1adf11411e50050db30362b97855aa75e919a13
Lê Anh Minh <lamdev2104@gmail.com>
2026-06-02 18:57:56 +0700

    Create dalymmo.com.user.js

---
+++ dalymmo.com.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Auto Change Image Comic By LAMDev
+// @description  Auto Change Image Comic By LAMDev
+// @require      https://raw.githubusercontent.com/leanhminh2104/auto-change-image-comic/refs/heads/main/dalymmo.com-v2.js
+// @updateURL   https://raw.githubusercontent.com/leanhminh2104/auto-change-image-comic/refs/heads/main/dalymmo.com-v2.js
+// @downloadURL https://raw.githubusercontent.com/leanhminh2104/auto-change-image-comic/refs/heads/main/dalymmo.com-v2.js
+// @namespace    http://tampermonkey.net/
+// @version      2.4
+// @author       LAMDev
+// @match        *://*/*
+// @grant        none
+// @icon         https://i.pinimg.com/236x/26/fc/2f/26fc2faf0e4a6628341e1a6f975654e7.jpg
+// ==/UserScript==
