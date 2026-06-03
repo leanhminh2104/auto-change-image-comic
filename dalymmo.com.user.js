9cf3b18bfbc05d6e55e23bf9ab9dc43f7c5ebb20
Lê Anh Minh <lamdev2104@gmail.com>
2026-06-03 20:38:03 +0700

    Update dalymmo.com.user.js

--- dalymmo.com.user.js
+++ dalymmo.com.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Auto Change Image Comic By LAMDev
-// @description  Auto Change Image Comic By LAMDev
-// @require      https://raw.githubusercontent.com/leanhminh2104/auto-change-image-comic/refs/heads/main/dalymmo.com-v2.js
-// @updateURL   https://raw.githubusercontent.com/leanhminh2104/auto-change-image-comic/refs/heads/main/dalymmo.com-v2.js
-// @downloadURL https://raw.githubusercontent.com/leanhminh2104/auto-change-image-comic/refs/heads/main/dalymmo.com-v2.js
 // @namespace    http://tampermonkey.net/
 // @version      2.4
+// @description  Auto Change Image Comic By LAMDev - Tự động nối chương đọc truyện tranh
 // @author       LAMDev
 // @match        *://*/*
 // @grant        none
-// @icon         https://i.pinimg.com/236x/26/fc/2f/26fc2faf0e4a6628341e1a6f975654e7.jpg
+// @icon         https://i.pinimg.com/736x/4d/c8/f5/4dc8f5522fe17a83d7f38b821a6d0042.jpg
+// @require      https://raw.githubusercontent.com/leanhminh2104/auto-change-image-comic/main/dalymmo.com-v2.js
+// @updateURL    https://raw.githubusercontent.com/leanhminh2104/auto-change-image-comic/main/dalymmo.com-v2.js
+// @downloadURL  https://raw.githubusercontent.com/leanhminh2104/auto-change-image-comic/main/dalymmo.com-v2.js
+// @run-at       document-idle
 // ==/UserScript==
